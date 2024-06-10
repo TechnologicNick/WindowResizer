cde48c9b077285693a80e2fe3a92be19fba02819
TechnologicNick <technologicnick@gmail.com>
2024-06-10 02:38:11 +0200

    Update assembly information

--- WindowResizer/My Project/AssemblyInfo.vb
+++ WindowResizer/My Project/AssemblyInfo.vb
@@ -1,4 +1,5 @@
-﻿Imports System
+﻿Imports System.Resources
+Imports System
 Imports System.Reflection
 Imports System.Runtime.InteropServices
 
@@ -9,10 +10,10 @@ Imports System.Runtime.InteropServices
 ' Review the values of the assembly attributes
 
 <Assembly: AssemblyTitle("WindowResizer")>
-<Assembly: AssemblyDescription("")>
-<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyDescription("A lightweight Visual Basic .NET application to resize and move your windows.")>
+<Assembly: AssemblyCompany("TechnologicNick")>
 <Assembly: AssemblyProduct("WindowResizer")>
-<Assembly: AssemblyCopyright("Copyright ©  2018")>
+<Assembly: AssemblyCopyright("© TechnologicNick 2018-2024")>
 <Assembly: AssemblyTrademark("")>
 
 <Assembly: ComVisible(False)>
@@ -33,3 +34,4 @@ Imports System.Runtime.InteropServices
 
 <Assembly: AssemblyVersion("1.0.0.0")>
 <Assembly: AssemblyFileVersion("1.0.0.0")>
+<Assembly: NeutralResourcesLanguage("en")>
